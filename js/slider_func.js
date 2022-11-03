035eb19e15f7b8d600f33125f81b752458c45d4b
yashaparajit <96583467+yashaparajit@users.noreply.github.com>
2022-11-03 20:01:45 +0530

    Add files via upload

---
+++ js/slider_func.js
@@ -0,0 +1,13 @@
+   $(document).on('ready', function(){
+        'use strict';
+        $('#layerslider').layerSlider({
+            autoStart: true,
+			responsive: true,
+			responsiveUnder: 1280,
+			layersContainer: 1170,
+			navButtons:false,
+			navStartStop:false,
+            skinsPath: 'layerslider/skins/'
+            
+        });
+    });// JavaScript Document
